aac73cf3a621d4343622c767dd30c123d8d7fda4
sandralin33 <114253510+sandralin33@users.noreply.github.com>
2024-11-17 23:15:51 -0800

    add mixer

--- Assets/Sandra/Scripts/Game/GameManager.cs
+++ Assets/Sandra/Scripts/Game/GameManager.cs
@@ -12,12 +12,14 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        startMusic = true;
+        
         if(startMusic) {
             music.Play();
         }
