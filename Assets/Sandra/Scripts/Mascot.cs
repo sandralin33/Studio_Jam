206ab771373f571b3df89fe778222573363e8716
Daderyn <kittyboobooloves@gmail.com>
2024-11-17 19:43:50 -0800

    Update Mascot.cs

--- Assets/Sandra/Scripts/Mascot.cs
+++ Assets/Sandra/Scripts/Mascot.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Mascot : MonoBehaviour
 {
+    
+    public Animator animator;
+
     // private bool Headbutt = false;
 
     // Start is called before the first frame update
